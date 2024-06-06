3c0597c7a928f9cdf8f690b14357b8c8561fba54
NansMarrey <2023-40219@liceocanadiense.edu.gt>
2024-06-06 14:12:43 -0600

    Se van a subir tres proyectos

---
+++ ejercicio2.py
@@ -0,0 +1,14 @@
+a=2
+b=4
+c=6
+d=8
+e=10
+s=a+b+c+d+e
+r=a-b-c-d-e
+m=a*b*c*d*e
+x=a/b/c/d/e
+
+print("La suma de los valores es igual a", s)
+print("La resta de los valores es igual a", r)
+print("La multiplicacion de los valores es igual a", m)
+print(f"La división de los valores es igual a {x: .4f}")
